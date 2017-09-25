209834b159442b9c10e0377dc14c019aa1100a91
sharmila1025 <sharmila@fusemachines.com>
2017-09-25 11:06:17 +0545

    address parsing in glassdoorcrawler

--- src/main/java/com/sharmila/scrapper/GlassdoorCrawler.java
+++ src/main/java/com/sharmila/scrapper/GlassdoorCrawler.java
@@ -12,6 +12,7 @@ import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.jsoup.Connection.Method;
@@ -19,10 +20,14 @@ import org.jsoup.Connection.Response;
 
 public class GlassdoorCrawler extends Thread {
 
+	public String htmlToText(String html) {
+		return Jsoup.parse(html).text();
+	}
+
 	public void run() {
 		System.setProperty("http.proxyHost", "205.189.37.86");
 		System.setProperty("http.proxyPort", "53281");
-		Map<String,String> jobMap=new HashMap<>();
+		Map<String, String> jobMap = new HashMap<>();
 		try {
 			Response response = Jsoup.connect("https://www.glassdoor.com/Job/jobs.htm")
 
@@ -33,50 +38,61 @@ public class GlassdoorCrawler extends Thread {
 
 					.followRedirects(true).execute();
 
-//			System.out.println(response.statusMessage());
-//
-//			System.out.println(response.url());
+			// System.out.println(response.statusMessage());
+			//
+			// System.out.println(response.url());
 			;
 			Document document = response.parse();
 			Elements elements = document.select("#JobResults ul.jlGrid li.jl");
 
 			for (Element e : elements) {
 				jobMap.put(e.getElementsByTag("a").attr("href"), e.getElementsByClass("minor").text());
-//				System.out.println(e.getElementsByTag("a").attr("href") + "-------- time ------"
-//						+ e.getElementsByClass("minor").text());
+				// System.out.println(e.getElementsByTag("a").attr("href") +
+				// "-------- time ------"
+				// + e.getElementsByClass("minor").text());
 			}
-			
-			
-			for(Map.Entry<String, String> map:jobMap.entrySet()){
-				System.out.println("https://www.glassdoor.com"+map.getKey());
-				Response response1=Jsoup.connect("https://www.glassdoor.com"+map.getKey()).userAgent("Mozilla/5.0").timeout(10 * 1000).execute();
-				Document document2=response1.parse();
-				
-				System.out.println(response1.url());
-				
-				Elements elements2=document2.select("article#MainCol");
-			
-				for (Element element :elements2 ){ 
-				
+
+			for (Map.Entry<String, String> map : jobMap.entrySet()) {
+				System.out.println("https://www.glassdoor.com" + map.getKey());
+				Response response1 = Jsoup.connect("https://www.glassdoor.com" + map.getKey()).userAgent("Mozilla/5.0")
+						.timeout(10 * 1000).execute();
+				Document document2 = response1.parse();
+
+				// System.out.println(response1.url());
+
+				Elements elements2 = document2.select("article#MainCol");
+
+				for (Element element : elements2) {
+
 					Elements sc = element.getElementsByTag("script");
-					String json=sc.html();
-					System.out.println("the text "+json);
-				//	System.out.println(json.replace("<script type='application/ld+json'>", "").replace("</script>", ""));
-					String newJson=json.replace("<script type='application/ld+json'>", "").replace("</script>", "");
-					System.out.println("------"+newJson);
-//				try {
-//					JSONObject jsonObject=new JSONObject(newJson);
-//				} catch (JSONException e1) {
-//					// TODO Auto-generated catch block
-//					e1.printStackTrace();
-//				}
-//					
-//					System.out.println(jsonObject.getString("title"));
-//					System.out.println(jsonObject.getString("datePosted"));
-//					System.out.println(jsonObject.getString("employmentType"));
-//					
-					
-			  }
+					String jsonBeforeParse = sc.html();
+
+					String jsonAfterParse = htmlToText(jsonBeforeParse);
+					// System.out.println(jsonAfterParse);
+
+					try {
+						System.out.println("after parsing : "+jsonAfterParse);
+						JSONObject jsonObject = new JSONObject(jsonAfterParse);
+						System.out.println(jsonObject.getString("title"));
+						System.out.println(jsonObject.getString("datePosted"));
+						System.out.println(jsonObject.getString("employmentType"));
+						
+						JSONObject jobLocation=(JSONObject) jsonObject.get("jobLocation");
+						
+						
+						System.out.println("location type :"+jobLocation.getString("@type"));
+						JSONObject addressParse=(JSONObject) jobLocation.get("address");
+						System.out.println("address type  "+addressParse.getString("@type"));
+						System.out.println("locality  "+addressParse.getString("addressLocality"));
+						System.out.println("region  "+addressParse.getString("addressRegion"));
+						
+						
+					} catch (JSONException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+
+				}
 				try {
 					Thread.sleep(11000);
 				} catch (InterruptedException e) {
@@ -87,9 +103,8 @@ public class GlassdoorCrawler extends Thread {
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
-		} 
+		}
 
-		
 	}
 
 	@Override
